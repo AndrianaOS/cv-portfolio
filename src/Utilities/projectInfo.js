bfb7209df54e355e3211c89c8e716a886c924ef3
Andriana <andrianasaffo@gmail.com>
2023-07-04 13:15:55 +0100

    Create projectInfo.js
    Create file that contains projects in an array of objects. import images from assets folder

---
+++ src/Utilities/projectInfo.js
@@ -0,0 +1,71 @@
+import pokedex from "../Assets/pokedex.png";
+import chatApp from "../Assets/chat app.png";
+import soulful from "../Assets/soulful.png";
+import quote from "../Assets/quote-generator.png";
+import hotel from "../Assets/hotel.png";
+import babyNest from "../Assets/baby-nest.png";
+import vlog from "../Assets/vlog.png";
+
+const projects = [
+  {
+    id: 1,
+    imgSrc: pokedex,
+    alt: "pokedex",
+    title: "Pokedex",
+    link: "https://andriana-pokedex.netlify.app/",
+    description: "Frontend webpage using React",
+  },
+  {
+    id: 2,
+    imgSrc: quote,
+    alt: "quote",
+    title: "Quote Generator",
+    link: "https://andriana-quote-generator.onrender.com",
+    description:
+      "Frontend, backend and database webpage using React, Node.JS and PostgreSQL",
+  },
+  {
+    id: 3,
+    imgSrc: chatApp,
+    alt: "chat",
+    title: "Hub Chat",
+    link: "https://chat-website-oerg.onrender.com",
+    description: "Frontend and backend webpage using React and Node.JS",
+  },
+  {
+    id: 4,
+    imgSrc: soulful,
+    alt: "affirmation",
+    title: "Soulful Truths",
+    link: "https://soulful-truths.netlify.app",
+    description: "Frontend webpage using HTML, CSS and JavaScript",
+  },
+  {
+    id: 5,
+    imgSrc: hotel,
+    alt: "hotel",
+    title: "Andriana Hotel",
+    link: "https://cyf-andriana-hotel-react.netlify.app",
+    description: "Frontend and backend webpage using React and Node.JS",
+  },
+  {
+    id: 6,
+    imgSrc: vlog,
+    alt: "vlog",
+    title: "Rap N Blues",
+    link: "https://video-blog.onrender.com",
+    description:
+      "Frontend, backend and database webpage using React, Node.JS and PostgreSQL",
+  },
+  {
+    id: 7,
+    imgSrc: babyNest,
+    alt: "babyNest",
+    title: "Baby Name Nest",
+    link: "https://video-blog.onrender.com",
+    description:
+      "Frontend, backend and database webpage using React, Node.JS and PostgreSQL",
+  },
+];
+
+export default projects;
