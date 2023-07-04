cb8de2862bdb4855b1494a6876923c7023e82379
Andriana <andrianasaffo@gmail.com>
2023-07-04 14:49:14 +0100

    JS file created for jobs and volunteering
    created jobs and volunteering js files in utilities folder which has array of objects of informations. Keeps components small and precise

---
+++ src/Utilities/jobsInfo.js
@@ -0,0 +1,50 @@
+const jobs = [
+  {
+    id: 1,
+    date: "11/2019 to 12/2021",
+    company: "CHL Mortgages",
+    role: "Arrears Operations Specialist",
+    duties: [
+      "Contacting customers by telephone regarding arrears on mortgage accounts and dealing with any queries",
+      "Taking payments for accounts that are in arrears and analysis of finances to work out repayment plans.",
+    ],
+  },
+  {
+    id: 2,
+    date: "02/2018 to 10/2019",
+    company: "Metro Bank",
+    role: "Customer Service Representative",
+    duties: [
+      "Onboarding new personal and business customers providing lending facilities and dealing with queries, customer service duties and ATM balancing.",
+    ],
+  },
+  {
+    id: 3,
+    date: "10/2017 to 02/2018 ",
+    company: "Metro Bank",
+    role: "Lead Cashier",
+    duties: [
+      "Supporting the cashiers in their tasks by performing observations, coaching sessions and till checks to improve service as well as handling cash orders and transfers for branch.",
+    ],
+  },
+  {
+    id: 4,
+    date: "10/2016 to 10/2017 ",
+    company: "Metro Bank",
+    role: "Cashier",
+    duties: [
+      "Customer service queries, cash handling and training on new cashier staff",
+    ],
+  },
+  {
+    id: 5,
+    date: "11/2015 to 10/2016",
+    company: "River Island",
+    role: "Retail sales assistant",
+    duties: [
+      "Dealing with customer queries and handling financial transactions.",
+    ],
+  },
+];
+
+export default jobs;
---
+++ src/Utilities/volunteeringInfo.js
@@ -0,0 +1,38 @@
+const volunteering = [
+  {
+    id: 1,
+    date: "06/2015 to 07/2015",
+    company: "Winterbourne Junior Girls' School ",
+    role: "Teaching Assistant",
+  },
+
+  {
+    id: 2,
+    date: "01/2015 to 02/2015",
+    company: "HM Revenue and Customs, Security and Information Directorate",
+    role: "Business Management Team trainee",
+  },
+
+  {
+    id: 3,
+    date: "11/2014 to 12/2014",
+    company: "Department for Work and Pensions",
+    role: "Business Support Team trainee",
+  },
+
+  {
+    id: 4,
+    date: "01/2014 to 03/2014",
+    company: "Pioneer Advertising/ Miami Marketing",
+    role: "Sales Representative",
+  },
+
+  {
+    id: 5,
+    date: "07/2012 to 08/2012",
+    company: "Ghana Standards Authority",
+    role: "Forensic, Drugs & Cosmetics Laboratory Assistant",
+  },
+];
+
+export default volunteering;
