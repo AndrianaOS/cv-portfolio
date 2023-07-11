518e4e34d13cfde12bdf511c3e3e1427e436837b
Andriana <andrianasaffo@gmail.com>
2023-07-11 10:38:42 +0100

    Update NavBar.jsx
    Anchor tags changed to Link for page navigation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../NavBar/NavBar.css";
 import "animate.css";
 
@@ -16,21 +17,21 @@ function NavBar(props) {
         />
       </section>
       <section className="menu-items ">
-        <a href="/" className="list-item">
+        <Link to="/" className="list-item">
           Home
-        </a>
-        <a href="/about" className="list-item">
+        </Link>
+        <Link to="/about" className="list-item">
           About Me
-        </a>
-        <a href="/projects" className="list-item">
+        </Link>
+        <Link to="/projects" className="list-item">
           Projects
-        </a>
-        <a href="/skills" className="list-item">
+        </Link>
+        <Link to="/skills" className="list-item">
           Skills
-        </a>
-        <a href="/jobs" className="list-item">
+        </Link>
+        <Link to="/jobs" className="list-item">
           Employment
-        </a>
+        </Link>
       </section>
     </nav>
   );
