882eb5b2be0f7204544ce7f582666f0b2e74aa94
Andriana <andrianasaffo@gmail.com>
2023-07-11 10:38:13 +0100

    Update About.jsx
    Sections changed to ul and p tags changed to li. Anchor tag changed to Link component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
 import "../About/About.css";
 
 function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <section className="info">
-        <p>
+      <ul className="info">
+        <li>
           I am a full time mum currently enrolled in an intensive software
           development course that I organize around my child's needs.
-        </p>
-        <p>
+        </li>
+        <li>
           I have always been fascinated by coding and my journey of learning to
           code began during lockdown. I learnt HTML and CSS by myself and fell
           in love with being able to build something that I can instantly see
@@ -17,21 +18,21 @@ function About() {
           needed to learn more to improve the skills I had began to learn.
           Additionally I learnt JavaScript, React, Node.JS and SQL through the
           bootcamp.
-        </p>
-        <p>
+        </li>
+        <li>
           I have an interest in learning new things and developing my skills and
           knowledge. Using the skills and knowledge I have acquired so far, i
           have built a number of webpages ranging from frontend projects to full
           stack projects.
-        </p>
-        <p>
+        </li>
+        <li>
           I have a background in the financial sector as well a degree in
           Forensic Science.
-        </p>
-      </section>
-      <a href="/" className="list-item">
+        </li>
+      </ul>
+      <Link to="/" className="list-item">
         Back
-      </a>
+      </Link>
     </div>
   );
 }
