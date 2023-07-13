85e25d9de828be5af5d042ee3a3fe0269e3f690e
Andriana <andrianasaffo@gmail.com>
2023-07-13 15:49:07 +0100

    Create edu.js
    education section created as an array of objects which is imported into Jobs component

---
+++ src/Utilities/edu.js
@@ -0,0 +1,23 @@
+const edu = [
+  {
+    id: 1,
+    date: "09/2022 - To Present",
+    institution: "Code Your Future (CYF)",
+    subject: "Software Development Course",
+    details: [
+      "Learnt various programming languages to build webpages ranging from frontend to fullstack",
+      "In team projects, managed the team in the role of Project Manager as well as a developer using Agile Methodologies",
+      "As Project Manager, drew out a project delivery plan and wireframe designs using Excel and Figma respectively",
+      "Managed workflows and merge conflicts",
+      "Shadowed a CYF product owner to learn more about the role as part of a Product Management Pathway",
+    ],
+  },
+  {
+    id: 2,
+    date: "09/2010 - 06/2013",
+    institution: "London South Bank University",
+    subject: "BSc (Hons) Forensic Science",
+  },
+];
+
+export default edu;
