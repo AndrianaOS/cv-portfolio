a68c632d634f7588e29dadc09aff31f81b8488cc
Andriana <andrianasaffo@gmail.com>
2023-07-13 11:59:09 +0100

    Update FrontPage.jsx
    buttonInfo array imported into FrontPage component making code shorter and readable. details tag added for a more interactive element

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "../FrontPage/FrontPage.css";
 import LinkButtons from "../LinkButtons/LinkButtons";
 import profilePic from "../../Assets/profilepic.jpeg";
+import buttonNames from "../../Utilities/buttonInfo";
 
 function FrontPage() {
   return (
@@ -10,20 +11,25 @@ function FrontPage() {
         <img src={profilePic} alt="profile" className="profile-pic" />
         <div className="profile-info">
           <h2 className="intro">Hello, My Name is Andriana Saffo</h2>
-          <p>Full time mum, Coding enthusiast</p>
+          <details>
+            <summary className="intro describe">I'm a ...</summary>
+            <ul>
+              <li>Full time mum</li>
+              <li>Coding enthusiast</li>
+              <li>Forensic Science Graduate</li>
+              <li>Product Management trainee</li>
+              <li>Career Switcher</li>
+              <li>Mentor</li>
+            </ul>
+          </details>
           <aside className="btn-info">
-            <Link to="/about">
-              <LinkButtons name="About me" />
-            </Link>
-            <Link to="/projects">
-              <LinkButtons name="Technical Projects" />
-            </Link>
-            <Link to="/skills">
-              <LinkButtons name="Skills" />
-            </Link>
-            <Link to="/jobs">
-              <LinkButtons name="Employment" />
-            </Link>
+            {buttonNames.map((eachButton) => {
+              return (
+                <Link to={eachButton.linkTo}>
+                  <LinkButtons name={eachButton.name} />
+                </Link>
+              );
+            })}
           </aside>
         </div>
       </section>
