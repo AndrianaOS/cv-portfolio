095580784028580f35e76abe478cf40721a0d20c
Andriana <andrianasaffo@gmail.com>
2023-07-13 11:57:19 +0100

    Create buttonInfo.js
    Array created for LinkButton component to prevent repetition in FrontPage component

---
+++ src/Utilities/buttonInfo.js
@@ -0,0 +1,20 @@
+const buttonNames = [
+  {
+    linkTo: "/about",
+    name: "About me",
+  },
+  {
+    linkTo: "/projects",
+    name: "Technical Projects",
+  },
+  {
+    linkTo: "/skills",
+    name: "Skills",
+  },
+  {
+    linkTo: "/jobs",
+    name: "Employment",
+  },
+];
+
+export default buttonNames;
