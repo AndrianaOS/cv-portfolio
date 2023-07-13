04b5dc19dc47ee530acfee4d23e0e694973ab11b
Andriana <andrianasaffo@gmail.com>
2023-07-13 15:57:51 +0100

    Update buttonInfo.js
    ids added to button information

--- src/Utilities/buttonInfo.js
+++ src/Utilities/buttonInfo.js
@@ -1,19 +1,23 @@
 const buttonNames = [
   {
+    id: 1,
     linkTo: "/about",
     name: "About me",
   },
   {
+    id: 2,
     linkTo: "/projects",
     name: "Technical Projects",
   },
   {
+    id: 3,
     linkTo: "/skills",
     name: "Skills",
   },
   {
+    id: 4,
     linkTo: "/jobs",
-    name: "Employment",
+    name: "Education & Employment",
   },
 ];
 
