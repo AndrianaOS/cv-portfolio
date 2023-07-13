3e09e6bf76d2232f0d7d6e4cc01dfee88183795a
Andriana <andrianasaffo@gmail.com>
2023-07-13 14:00:02 +0100

    Update Skills.jsx
    Skills edited to add some more

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -12,26 +12,28 @@ function Skills() {
             <li className="skills-list">HTML5</li>
             <li className="skills-list">CSS</li>
             <li className="skills-list">JavaScript</li>
-            <li className="skills-list">React</li>
+            <li className="skills-list">React.JS</li>
           </ul>
 
           <h3>Server Side</h3>
           <ul>
-            <li className="skills-list">SQL</li>
-            <li className="skills-list">Node</li>
+            <li className="skills-list">SQL particularly PostgreSQL</li>
+            <li className="skills-list">Node.JS with Express Framework</li>
           </ul>
         </section>
         <section className="tools">
           <h3>Tools</h3>
           <ul>
             <li className="skills-list">Git</li>
-            <li className="skills-list">GitHub</li>
+            <li className="skills-list">GitHub and GitHub Project Boards</li>
             <li className="skills-list">Virtual Studio Code</li>
             <li className="skills-list">Figma</li>
+            <li className="skills-list">Miro</li>
             <li className="skills-list">Slack</li>
             <li className="skills-list">Netlify</li>
             <li className="skills-list">Render</li>
             <li className="skills-list">Microsoft Office 365</li>
+            <li className="skills-list">Supabase</li>
           </ul>
         </section>
       </div>
@@ -39,6 +41,15 @@ function Skills() {
         <h2>Personal Attributes</h2>
         <section className="attributes">
           <ul>
+            <li className="skills-item">
+              Managed and contributed to the development of a group project
+              named
+              <a href="https://soulful-truths.netlify.app">
+                {" "}
+                Soulful Truths
+              </a>{" "}
+              in an Agile framework
+            </li>
             <li className="skills-item">
               Excellent listening, written and communication skills.
             </li>
