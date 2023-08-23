5427f25d0b8f85674e20fc5d84a1ac4e61767314
Andriana <andrianasaffo@gmail.com>
2023-08-23 12:10:15 +0100

    Update Projects.jsx
    Another section added to Projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -23,6 +23,36 @@ function Projects() {
           );
         })}
       </section>
+      <aside className="notes">
+        <h3>Notes about Projects</h3>
+        <ul>
+          <li>
+            The Quotes Generator originally did not include the database until I
+            learnt PostgreSQL. I then modified the project to include a
+            database. This is my 1st project that included a backend making GET
+            requests to the database.
+          </li>
+          <li>The Hub Chat project makes GET, POST and DELETE requests.</li>
+          <li>
+            Soulful Truths was my first team project where I was the project
+            manager as well as being responsible for the UX/UI and the Frontend.
+            This project helped me understand how to interact and work with
+            GitHub in a collaborative way to avoid merge conflicts.
+          </li>
+          <li>
+            The hotel project was the first big React project that had to be
+            completed over 3 weeks.
+          </li>
+          <li>
+            Rap and Blues project was an assessment during my time at the
+            bootcamp. This backend follows the CRUD method
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </aside>
       <Link to="/" className="list-item">
         Back
       </Link>
