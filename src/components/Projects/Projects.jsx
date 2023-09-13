ea834b3d520f20372cac2058475e48005a39a203
Andriana <andrianasaffo@gmail.com>
2023-09-13 11:38:34 +0100

    Update Projects.jsx
    Apply bold and italics to project names

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -27,48 +27,80 @@ function Projects() {
         <h2>Notes about Projects</h2>
         <ul>
           <li>
-            The Quotes Generator originally did not include the database until I
-            learnt PostgreSQL. I then modified the project to include a
-            database. This is my 1st project that included a backend making GET
-            requests to the database.
-          </li>
-          <li>The Hub Chat project makes GET, POST and DELETE requests.</li>
-          <li>
-            Soulful Truths was my first team project where I was the project
-            manager as well as being responsible for the UX/UI and the Frontend.
-            This project helped me understand how to interact and work with
-            GitHub in a collaborative way to avoid merge conflicts.
+            The{" "}
+            <b>
+              <i>Quotes Generator</i>
+            </b>{" "}
+            originally did not include the database until I learnt PostgreSQL. I
+            then modified the project to include a database. This is my 1st
+            project that included a backend making GET requests to the database.
           </li>
           <li>
-            The hotel project was the first big React project that had to be
-            completed over 3 weeks.
+            The{" "}
+            <b>
+              <i>Hub Chat</i>
+            </b>{" "}
+            project makes GET, POST and DELETE requests.
           </li>
           <li>
-            Rap and Blues project was an assessment during my time at the
-            bootcamp. This backend follows the CRUD method
+            <b>
+              <i>Soulful Truths</i>
+            </b>{" "}
+            was my first team project where I was the project manager as well as
+            being responsible for the UX/UI and the Frontend. This project
+            helped me understand how to interact and work with GitHub in a
+            collaborative way to avoid merge conflicts.
           </li>
           <li>
-            Baby Name Nest was my own take on a React challenge which gave a
-            json file with names. I decided to make it a challenge for myself to
-            include a database and unisex option. Each sex option gives the name
-            a different background color when chosen
+            The{" "}
+            <b>
+              <i>Hotel</i>
+            </b>{" "}
+            project was the first big React project that had to be completed
+            over 3 weeks.
           </li>
           <li>
-            Wix clone was an HTML and CSS challenge to try and recreate a
-            resume. This helped me about having a plan of how to tackle moving
-            parts on a page instead of just jumping in to start coding
+            <b>
+              <i>Rap and Blues</i>
+            </b>{" "}
+            project was an assessment during my time at the bootcamp. This
+            backend follows the CRUD method
           </li>
           <li>
-            Good PR is an application that pulls information from GitHub to
-            track team members participating in the final project at Code Your
-            Future. This was to ensure that members were contributing fairly to
-            the project. During this project, I acted in the capacity of a
-            Technical lead and a Full Stack Developer
+            <b>
+              <i>Baby Name Nest</i>
+            </b>{" "}
+            was my own take on a React challenge which gave a json file with
+            names. I decided to make it a challenge for myself to include a
+            database and unisex option. Each sex option gives the name a
+            different background color when chosen
           </li>
           <li>
-            The Visual Calendar was a presentation I created to solve what I saw
-            as a gap in regards to booking meetings and finding out peoples
-            schedules and availabilities.
+            <b>
+              <i>Wix clone</i>
+            </b>{" "}
+            was an HTML and CSS challenge to try and recreate a resume. This
+            helped me about having a plan of how to tackle moving parts on a
+            page instead of just jumping in to start coding
+          </li>
+          <li>
+            <b>
+              <i>Good PR</i>
+            </b>{" "}
+            is an application that pulls information from GitHub to track team
+            members participating in the final project at Code Your Future. This
+            was to ensure that members were contributing fairly to the project.
+            During this project, I acted in the capacity of a Technical lead and
+            a Full Stack Developer
+          </li>
+          <li>
+            The{" "}
+            <b>
+              <i>Visual Calendar</i>
+            </b>{" "}
+            was a presentation I created to solve what I saw as a gap in regards
+            to booking meetings and finding out peoples schedules and
+            availabilities in the organization.
           </li>
         </ul>
       </aside>
