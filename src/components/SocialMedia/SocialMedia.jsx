368f17ae4b9e00863de74b22da458e6caf02f946
Andriana <andrianasaffo@gmail.com>
2023-09-15 21:58:35 +0100

    adjust CSS
    adjust footer and landing page sizing

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -4,10 +4,10 @@ import linkedin from "../../Assets/linkedin.png";
 function SocialMedia() {
   return (
     <div className="social">
-      <a href="https://www.linkedin.com/in/andriana-saffo/">
+      <a href="https://www.linkedin.com/in/andriana-saffo/" className="linkedin">
         <img src={linkedin} alt="linkedIn" />
       </a>
-      <a href="https://github.com/AndrianaOS">
+      <a href="https://github.com/AndrianaOS" className="github">
         <img src={github} alt="github" />
       </a>
     </div>
